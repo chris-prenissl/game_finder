d5681fa24d8b31d8af4507b191204cdd76c96a90
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-21 20:19:39 +0200

    Create a hive repository for favorites

---
+++ lib/data/repository/favorite_repository.dart
@@ -0,0 +1,24 @@
+import 'package:hive/hive.dart';
+
+class FavoriteRepository {
+
+  Future<void> storeGameFavoriteId(int id) async {
+    final box = await Hive.openBox(_favoriteTable);
+
+    box.put(id, true);
+  }
+
+  Future<void> removeGameFavoriteId(int id) async {
+    final box = await Hive.openBox(_favoriteTable);
+
+    box.delete(id);
+  }
+
+  Future<bool> isFavorite(int id) async {
+    final box = await Hive.openBox(_favoriteTable);
+
+    return box.get(id, defaultValue: false);
+  }
+
+  static const String _favoriteTable = 'favorite';
+}
