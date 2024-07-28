792060a0d6b2d72ff8d879760cd2206619013b79
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-29 00:57:50 +0200

    Implement better search button

--- lib/presentation/screen/game_image_ai_capture_screen.dart
+++ lib/presentation/screen/game_image_ai_capture_screen.dart
@@ -28,73 +28,75 @@ class _GameImageAiCaptureScreenState extends State<GameImageAiCaptureScreen> {
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<GameImageAiCaptureBloc>();
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Capture game content to search for the game'),
-        ),
-        body: BlocConsumer<GameImageAiCaptureBloc, GameImageAiCaptureState>(
-          listener: (context, state) {
-            state.when(
-              loading: () {},
-              initialized: (camera) {
-                _controller = CameraController(camera, ResolutionPreset.medium);
-                _controller!.initialize().then((_) {
-                  if (!mounted) {
-                    return;
-                  }
-                  setState(() {});
-                }).catchError((Object e) {
-                  if (e is CameraException) {
-                    switch (e.code) {
-                      case _cameraAccessDeniedErrorCode:
-                      // Handle access errors here.
-                        break;
-                      default:
-                      // Handle other errors here.
-                        break;
+    return BlocConsumer<GameImageAiCaptureBloc, GameImageAiCaptureState>(
+      listener: (context, state) {
+        state.when(
+          loading: () {},
+          initialized: (camera) {
+            _controller = CameraController(camera, ResolutionPreset.medium);
+            _controller!.initialize().then((_) {
+              if (!mounted) {
+                return;
+              }
+              setState(() {});
+            }).catchError((Object e) {
+              if (e is CameraException) {
+                switch (e.code) {
+                  case _cameraAccessDeniedErrorCode:
+                    // Handle access errors here.
+                    break;
+                  default:
+                    // Handle other errors here.
+                    break;
+                }
+              }
+            });
+          },
+          aiResult: (gameTitle) {
+            context.pop(gameTitle);
+          },
+          error: (_) {},
+        );
+      },
+      builder: (context, state) {
+        return state.when(
+          initialized: (_) => Scaffold(
+            appBar: AppBar(
+              title: const Text(_gameSearchTitle),
+            ),
+            body: Center(
+              child: _controller != null
+                  ? CameraPreview(_controller!)
+                  : const CircularProgressIndicator(),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: FloatingActionButton.large(
+              onPressed: _controller != null
+                  ? () async {
+                      final image = await _captureImage();
+                      bloc.add(
+                        GameImageAiCaptureEvent.imageCapture(image),
+                      );
                     }
-                  }
-                });
-              },
-              aiResult: (gameTitle) {
-                context.pop(gameTitle);
-              },
-              error: (_) {},
-            );
-          },
-          builder: (context, state) {
-            return state.when(
-                initialized: (_) =>
-                    Center(
-                      child: _controller != null
-                          ? CameraPreview(_controller!)
-                          : const CircularProgressIndicator(),
-                    ),
-                aiResult: (_) =>
-                const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                loading: () =>
-                const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                error: (message) =>
-                    Center(
-                      child: Text(
-                        message,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                    )
-            );
-          },
-        ),
-        floatingActionButton: _controller != null ? FloatingActionButton(
-          onPressed: () async {
-            final image = await _captureImage();
-            bloc.add(GameImageAiCaptureEvent.imageCapture(image));
-          },
-          child: const Icon(Icons.camera_alt),
-        ) : null
+                  : null,
+              child: const Icon(Icons.auto_awesome),
+            ),
+          ),
+          aiResult: (_) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          error: (message) => Center(
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.red),
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -110,5 +112,6 @@ class _GameImageAiCaptureScreenState extends State<GameImageAiCaptureScreen> {
     super.dispose();
   }
 
+  static const _gameSearchTitle = 'Search with Image';
   static const _cameraAccessDeniedErrorCode = 'CameraAccessDenied';
 }
