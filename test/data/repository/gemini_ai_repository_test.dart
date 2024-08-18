f70573125f717e50e28ddc088b289c7cd2a2957f
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-18 09:45:33 +0200

    Add test for getDescriptionOfGame

--- test/data/repository/gemini_ai_repository_test.dart
+++ test/data/repository/gemini_ai_repository_test.dart
@@ -6,15 +6,31 @@ import 'package:game_finder/data/repository/gemini_ai_repository.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  late String apiKey;
+  late GenerativeModel model;
+  late GeminiAiRepository geminiRepository;
+
+  setUp(() async {
+    await dotenv.load(fileName: ".env");
+
+    apiKey = dotenv.get('GEMINI_API_KEY');
+    model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
+    geminiRepository = GeminiAiRepository(generativeModel: model);
+  });
   
   test('getGameNameByImage', () async {
-    final apiKey = dotenv.get('GEMINI_API_KEY');
-    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
-    final geminiRepository = GeminiAiRepository(generativeModel: model);
-
     final image = await File('screenshots/search.png').readAsBytes();
     final result = await geminiRepository.getGameNameByImage(image);
+
+    expect(result, isNotEmpty);
+  });
+
+  test('getDescriptionOfGame', () async {
+    final stream = geminiRepository.getDescriptionOfGame('Game');
+    String result = "";
+    await for (final response in stream) {
+      result += response;
+    }
 
     expect(result, isNotEmpty);
   });
